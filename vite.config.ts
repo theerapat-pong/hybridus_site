bcdde516dd25702b2ca1393cc60d9801fd678331
Aung Kyaw Min <theerapat.mdm@gmail.com>
2025-07-17 14:02:21 +0700

    Fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      // Vite replaces this with the actual value during build, e.g., 'process.env.API_KEY': '"your-api-key"'
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+  }
 })
